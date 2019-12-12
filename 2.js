5009b6db900810842d2434ee00470f5fb0dedaf3
Rachel Gould <rachel@rachelgould.dev>
2019-12-12 10:15:21 -0800

    refactor both solutions

--- 2.js
+++ 2.js
@@ -2,22 +2,19 @@
 function findClosestValueInBst(tree, target) {
   let closest = tree.value;
   function checkNode(node) {
-    function checkIsCloser(node) {
-      if (Math.abs(target - node.value) < Math.abs(target - closest)) {
-        closest = node.value;
-      }
-    }
+    if (!node) return;
     if (node.value === target) {
       closest = node.value;
     } else if (node.value > target) {
       checkIsCloser(node)
-      if (node.left) {
-        checkNode(node.left)
-      }
+      checkNode(node.left)
     } else {
       checkIsCloser(node)
-      if (node.right) {
-        checkNode(node.right)
+      checkNode(node.right)
+    }
+    function checkIsCloser(node) {
+      if (Math.abs(target - node.value) < Math.abs(target - closest)) {
+        closest = node.value;
       }
     }
   }
