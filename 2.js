5de65a6e00c726ece2e22b9d6a14d6d0f1f133e5
Rachel Gould <rachel@rachelgould.dev>
2019-12-12 09:12:56 -0800

    puzzling it out

--- 2.js
+++ 2.js
@@ -1,4 +1,37 @@
 // Solution 1:
 function findClosestValueInBst(tree, target) {
-  
+  let bstNodes = [];
+  // Check if left and right are BST
+  function checkLeft(current, next) {
+    console.log("-------------Checking left on :", current)
+    console.log("Next is :", current)
+    next === null ? true : 
+                          (current.value > next.value) && checkChildren(current)
+                          console.log("done-----------")
+  }
+  function checkRight(current, next) {
+    console.log("-------------Checking right on :", current)
+    console.log("Next is :", current)
+    next === null ? true : 
+                           (current.value <= next.value) && checkChildren(current)
+                           console.log("done-----------")
+  }
+  function checkChildren(current) {
+    console.log("-----------Checking Children---------")
+    if (checkLeft(current.left) && checkRight(current.right)) {
+      bstNodes.push(current.value);
+      console.log("Pushed new value to bst nodes list")
+      console.log("BST NODES:::: ", bstNodes)
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function checkBst(node) {
+    checkChildren(node)
+    console.log("Done in original function")
+    console.log("BST NODES:::: ", bstNodes)
+  }
+
+  checkBst(tree);
 }
