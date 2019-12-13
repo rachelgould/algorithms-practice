eba5a1e25eb64dae800ca54a78eb8c376742908d
Rachel Gould <rachel@rachelgould.dev>
2019-12-13 12:08:15 -0800

    solved

--- 3.js
+++ 3.js
@@ -1,3 +1,18 @@
 function branchSums(root) {
+  let sums = [];
+  calculateBranchSums(root, 0, sums);
+  return sums;
   
+  function calculateBranchSums(node, currentSum, sums) {
+    if (!node) {
+      return;
+    }
+    let newCurrentSum = currentSum + node.value;
+    if (!node.left && !node.right) {
+      sums.push(newCurrentSum);
+      return;
+    }
+    calculateBranchSums(node.left, newCurrentSum, sums);
+    calculateBranchSums(node.right, newCurrentSum, sums);
+  }
 }
