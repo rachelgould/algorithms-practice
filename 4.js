f77316b056f5dc6681d024fb3ddeceaa69a44784
Rachel Gould <rachel@rachelgould.dev>
2019-12-16 20:58:21 -0800

    solved

---
+++ 4.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+    this.children.forEach(c => {
+      c.depthFirstSearch(array)
+    });
+    return array;
+  }
+}
